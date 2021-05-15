bfab0d427df1e6efd96f1160a6ab4cfe5223c87b
Jihyun Yoon (Nick) <nickyoon89@gmail.com>
2021-05-15 17:49:59 -0400

    local post function

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,47 @@
+import {useState} from 'react';
 import './App.css';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ReactHtmlParser from 'react-html-parser';
 
 function App() {
+  const [twitterContent, setTwitterContent] = useState({
+    title: '',
+    content: '',
+  });
+
+  const [viewContent, setViewContent] = useState([]);
+
+  const getValue = e => {
+    const { name, value } = e.target;
+      setTwitterContent({
+        ...twitterContent,
+        [name]: value
+      })
+      console.log(twitterContent);
+  };
+
+
   return (
     <div className="App">
       <h1>Twitter Clone</h1>
-      <div className='movie-container'>
-        <h2>Title</h2>
-        <div>
-          Content
-        </div>
+      <div className='twitter-container'>
+        {viewContent.map(element => 
+          <div>
+            <h2>{element.title}</h2>
+            <div>
+              {ReactHtmlParser(element.content)}
+            </div>
+          </div>
+        )}
       </div>
       <div className='form-wrapper'>
-        <input className="title-input" type='text' placeholder='Title' />
+        <input 
+          className="title-input" 
+          type='text' 
+          placeholder='Title' 
+          onChange={getValue}
+          name='title' />
         <CKEditor
           editor={ClassicEditor}
           data="<p>Hello from CKEditor 5!</p>"
@@ -24,6 +52,11 @@ function App() {
           onChange={(event, editor) => {
             const data = editor.getData();
             console.log({ event, editor, data });
+            setTwitterContent({
+              ...twitterContent,
+              content: data
+            })
+            console.log(twitterContent);
           }}
           onBlur={(event, editor) => {
             console.log('Blur.', editor);
@@ -33,7 +66,11 @@ function App() {
           }}
         />
       </div>
-      <button className="submit-button">Post</button>
+      <button className="submit-button"
+      onClick={() => {
+        console.log(viewContent);
+        setViewContent(viewContent.concat({...twitterContent}));
+      }}>Post</button>
     </div>
   );
 }
